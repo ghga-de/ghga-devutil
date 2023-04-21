5f8c26df03420879616027d44faf4b64f1852c2f
Seyit Zor <seyitzor@gmail.com>
2023-04-21 10:13:11 +0200

    rename gdevutil to ghga-devutil

--- ghga_devutil/__init__.py
+++ ghga_devutil/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The package provides the functionality required for the gdevutil
+"""The package provides the functionality required for the ghga-devutil
 command line utility."""
 
 __version__ = "1.0.0"
